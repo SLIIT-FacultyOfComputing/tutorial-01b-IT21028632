13fc6251ec725aa85f77e70c641ce79bef8b157d
IT21028632 <87464248+IT21028632@users.noreply.github.com>
2021-07-16 02:23:23 +0000

    I did Exercise 1,2,3,4

--- Tute03.c
+++ Tute03.c
@@ -13,7 +13,18 @@ sum = 1+2+3+...+10 = 55 */
 
 #include <stdio.h>
 int main() {
-  
+    int number, sumOfNums = 0;
+    int i;
+
+    printf("Number : ");
+    scanf("%d", &number);
+
+    for (i = 1; i <= number; i++)
+    {
+        sumOfNums += i;
+    }
+
+    printf("Sum of 1 to %d = %d", number, sumOfNums);
   return 0;
 }
 
